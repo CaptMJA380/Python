ff1a9345cf3ef644d73c7a86e7dd0c4fdc80ea78
Mrunal Jadhav <120638782+CaptMJA380@users.noreply.github.com>
2025-08-26 22:52:47 +0530

    NetflixApp.py

--- NetflixApp.py
+++ NetflixApp.py
@@ -1,11 +1,33 @@
+user_name = input("Enter your name: ")
+user_password = input("Enter your password: ")
+
 print("Welcome! to Netflix App")
-c_username=input("Enter username: ")
-c_password=input("Enter your password: ")
-if c_username == user_name and c_password == user_password:
-  print("Login Sucessful!")
-  break
+
+for i in range(1,4):
+  c_username=input("Enter username: ")
+  c_password=input("Enter your password: ")
+  if c_username == user_name and c_password == user_password:
+    print("Login Sucessful!")
+    break
+  else:
+    print("Login failed!")
+    print("If you have forgotten your password, please reset it")
+    print("If you don't have an account, please sign up")
+  
+print("Welcome! to Netflix")
+print("Choose the plan that suits you best: \n 1. Basic Plan \n 2. Standard Plan \n 3. Preminum Plan")
+plan_chaoice = input("Enter the number option of your chosen plan")
+if plan_choice=="1":
+    print("You have chosen Basic Plan")
+elif plan_choice=="2":
+    print("You have chosen Standard Plan")
+elif plan_choice=="3":
+    print("You have chosen Premium Plan")
 else:
-  print("Login failed!")
-  print("If you have forgotten your password, please reset it")
-  print("If you don't have an account, please sign up")
+    print("Enter valid option!")
+
+movies = ["Inception","The Matrix","Interatellar","Fighter","Piku"]
+print("available movies: ")
+for movie in movies:
+  print(f"- {movies}")
   
